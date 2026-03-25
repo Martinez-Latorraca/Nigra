a0e0791855e2b10b9a2568e48b84ae64f77e3b17
Nicolas Martinez <52309078+Martinez-Latorraca@users.noreply.github.com>
2026-03-25 02:46:06 -0300

    feat: update coors to front deployment

--- server/index.js
+++ server/index.js
@@ -74,7 +74,7 @@ app.get(/.*/, (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://nigra-server.onrender.com/"],
+        origin: ["https://nigra.onrender.com/", "https://nigra-server.onrender.com/"],
         methods: ["GET", "POST"]
     }
 });
